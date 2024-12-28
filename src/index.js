4c049e77c0c46fa278e99e879e48f85b1acb0ff0
Abhishek Singh <abhisheksingh02431@gmail.com>
2024-12-28 19:09:27 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -12,7 +12,15 @@ import { DB_NAME } from "./constants.js";
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed!!! ", err);
 
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+// First we will see how can we write in promises
+const asyncHandler = (requestHandler) =>{
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+// Now we will see how can we write in try catch
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {} 
+// const asyncHandler = (func) => async () => {}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//        await fn(req, res, next) 
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+        
+//     }
+// }
